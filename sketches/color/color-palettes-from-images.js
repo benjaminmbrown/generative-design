c92ea9d0f0a7021e2b9f69d828f469bf4f805dc5
benjaminmbrown <benjaminbb@gmail.com>
2018-10-06 23:23:38 -0400

    add some libs and image processing

--- sketches/color/color-palettes-from-images.js
+++ sketches/color/color-palettes-from-images.js
@@ -1,7 +1,7 @@
 'use strict';
 var img;
 var colors = [];
-var sortMode;
+var sortMode = null;
 
 function preload(){
     img = loadImage('../../img/dj-colors-ben-brown.jpeg');
@@ -11,12 +11,51 @@ function preload(){
 function setup() {
   createCanvas(720, 720);
   noCursor();
-  image(img,0,0);
+  noStroke();
+  //image(img,0,0);
 }
 
 function draw(){
-    var tileCount = width/max(mouseX,5);
-    var rectSize = width/tileCount;
+    var tilecount = width/max(mouseX,5);
+    var rectSize = width/tilecount;
+    img.loadPixels();
+    colors = [];
 
-    var colors = new Array[tileCount*tileCount]
+    for(var gridY = 0; gridY < tilecount;gridY++){
+        for(var gridX = 0; gridX < tilecount; gridX++){
+            var px = gridX*rectSize;
+            var py = gridY*rectSize;
+            var i = (py*img.width + px) * 4;
+            var c = color(img.pixels[i], img.pixels[i+1],img.pixels[i+2],img.pixels[i+3]);
+            colors.push(c);
+        }
+    }
+
+    gd.sortColors(colors,sortMode);//generative design lib
+
+    var i=0;
+
+    for(var gridY = 0; gridY < tilecount;gridY++){
+        for(var gridX = 0; gridX < tilecount; gridX++){
+            fill(colors[i]);
+            rect(gridX*rectSize,gridY*rectSize,rectSize,rectSize);
+            i++;
+        }
+    }
 }
+
+function keyReleased() {
+    if (key == 'c' || key == 'C') writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
+    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  
+    if (key == '1') img = loadImage('../../img/dj-colors-ben-brown.jpeg');
+    if (key == '2') img = loadImage('../../img/2.jpeg');
+    if (key == '3') img = loadImage('../../img/3.jpeg');
+    if (key == '4') img = loadImage('../../img/4.jpeg');
+  
+    if (key == '5') sortMode = null;
+    if (key == '6') sortMode = gd.HUE;
+    if (key == '7') sortMode = gd.SATURATION;
+    if (key == '8') sortMode = gd.BRIGHTNESS;
+    if (key == '9') sortMode = gd.GRAYSCALE;
+  }
